7a832552e2d13ac430cc19feaa9e51c703350336
Ben N <ben@rigeltechnical.com>
2019-06-20 14:50:51 -0500

    Add a test that tries to access the compiler

--- core/src/test/kotlin/TestExecute.kt
+++ core/src/test/kotlin/TestExecute.kt
@@ -6,6 +6,7 @@ import io.kotlintest.matchers.collections.shouldContain
 import io.kotlintest.matchers.collections.shouldHaveSize
 import io.kotlintest.matchers.collections.shouldNotContain
 import io.kotlintest.matchers.doubles.shouldBeLessThan
+import io.kotlintest.matchers.string.contain
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
@@ -802,6 +803,7 @@ try {
         """.trim()).compile()
         }
     }
+
     "it should not allow snippets to read from the internet" {
         val executionResult = Source.fromSnippet("""
 
@@ -950,6 +952,34 @@ while (true) {
         val goodResult = goodTask.await()
         goodResult should haveOutput("AAAAAAAAAA")
     }
+
+    "f:should not allow access to the compiler" {
+        val result = Source.fromSnippet("""
+import java.lang.reflect.*;
+
+try {
+    Class<?> sourceClass = Class.forName("edu.illinois.cs.cs125.jeed.core.Source");
+    Field sourceCompanion = sourceClass.getField("Companion");
+    Class<?> snippetKtClass = Class.forName("edu.illinois.cs.cs125.jeed.core.SnippetKt");
+    Method fromSnippet = snippetKtClass.getMethod("fromSnippet", sourceCompanion.getType(), String.class, int.class);
+    Object snippet = fromSnippet.invoke(null, sourceCompanion.get(null), "System.out.println(403);", 4);
+    Class<?> snippetClass = snippet.getClass();
+    Class<?> compileArgsClass = Class.forName("edu.illinois.cs.cs125.jeed.core.CompilationArguments");
+    Method compile = Class.forName("edu.illinois.cs.cs125.jeed.core.CompileKt").getMethod("compile", sourceClass, compileArgsClass);
+    Object compileArgs = compileArgsClass.newInstance();
+    Object compiledSource = compile.invoke(null, snippet, compileArgs);
+    System.out.println(compiledSource);
+    System.out.println("Accessed compiler!");
+} catch (Exception e) {
+    e.printStackTrace();
+}
+
+//Source.fromSnippet("System.out.println(5);").compile().execute();
+            """.trim()).compile().execute()
+        println(result.output)
+        result.output shouldNot contain("!")
+    }
+
 })
 
 fun haveCompleted() = object : Matcher<ExecutionResult<out Any?>> {
