ffd347fc560a1dcd4ff05bf98294f07dedcde49c
Geoffrey Challen <challen@illinois.edu>
2019-08-23 16:52:36 -0500

    Working on frontend component.

--- react/example/src/App.js
+++ react/example/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 
-import JeedResult from 'jeed'
+import { JeedResult } from 'jeed'
 
 export default class App extends Component {
   render () {
     const backend = process.env.REACT_APP_JEED_BACKEND
     return (
       <div>
-        <JeedResult backend={ backend } />
-        <JeedResult backend={ backend } />
+        <JeedResult backend={ backend } job={{
+          tasks: [ "execute" ],
+          snippet: `System.out.println("Hello, world!");`
+        }} />
       </div>
     )
   }
--- react/src/index.js
+++ react/src/index.js
@@ -1,33 +1,99 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
 import axios from 'axios'
 
 const backends = {}
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+function jeedBackendWrapper (WrappedComponent) {
+  class JeedBackendWrapper extends Component {
+    constructor (props) {
+      super(props)
+      this.state = { connected: false, config: null }
+    }
+
+    componentDidMount() {
+      const { backend } = this.props
+      if (!(backend in backends)) {
+        backends[backend] = axios.get(backend)
+      }
+      backends[backend].then(config => {
+        this.setState({ connected: true, config })
+      })
+    }
+
+    render () {
+      const { connected, config } = this.state
+      return <WrappedComponent connected={connected} config={config} { ...this.props } />
+    }
   }
 
-  constructor (props) {
+  JeedBackendWrapper.displayName =
+    `JeedBackendWrapper(${
+      WrappedComponent.displayName || WrappedComponent.name || 'Component'
+    })`
+
+  return JeedBackendWrapper
+}
+
+class JeedResultInner extends Component {
+  constructor(props) {
     super(props)
-
-    this.state = { connected: false, config: null }
-
-    const { backend } = props
-    if (!(backend in backends)) {
-      backends[backend] = axios.get(backend)
-    }
-    backends[backend].then(config => { this.setState({ connected: true, config }) })
+    this.state = { request: null, result: null }
   }
 
-  render () {
-    const { connected, config } = this.state
-    if (!connected) {
-      return (<div>Waiting</div>)
+  componentDidUpdate() {
+    if (!this.props.connected) {
+      return
     }
-    return (
-      <div>{JSON.stringify(config, null, 2)}</div>
-    )
+    if (this.state.request) {
+      return
+    }
+
+    const { backend, job } = this.props
+    const request = run (backend, job)
+
+    this.setState({ request })
+    request.response.then(response => {
+      this.setState({ result: response.data })
+    }).catch(err => {
+      console.error(err)
+    })
+  }
+
+  componentWillUnmount() {
+    if (!this.state.request || this.state.response) {
+      return
+    }
+    this.state.request.cancel()
+  }
+
+  render() {
+    const { connected, config } = this.props
+    const { result } = this.state
+
+    if (!connected) {
+      const { connecting } = this.props
+      return ( connecting || <div>Connecting...</div> )
+    }
+    if (!result) {
+      return ( <pre>{ JSON.stringify(config, null, 2) }</pre> )
+    }
+    return ( <pre>{ JSON.stringify(result, null, 2) }</pre> )
   }
 }
+
+const JeedResult = jeedBackendWrapper(JeedResultInner)
+export { JeedResult }
+
+function run (backend, job) {
+  const cancellationHandle = axios.CancelToken.source()
+
+  return {
+    response: axios.post(backend, job, {
+      cancelToken: cancellationHandle.token
+    }),
+    cancel: () => {
+      cancellationHandle.cancel()
+    }
+  }
+}
+
