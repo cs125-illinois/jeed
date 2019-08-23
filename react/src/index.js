bcc405a57e907c2380b0c834bccb7c2793c474b1
Geoffrey Challen <challen@illinois.edu>
2019-08-23 08:47:19 -0500

    Starting React component.

---
+++ react/src/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class ExampleComponent extends Component {
+  static propTypes = {
+    text: PropTypes.string
+  }
+
+  render () {
+    const {
+      text
+    } = this.props
+
+    return (
+      <div>
+        Component: {text}
+      </div>
+    )
+  }
+}
