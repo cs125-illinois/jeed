632279c67ecd784d694677f086f1bc9de62a7cc9
Geoffrey Challen <challen@illinois.edu>
2019-08-23 18:40:33 -0500

    Backup ktor version to fix CORS.

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -8,7 +8,7 @@ tasks.test {
     systemProperties["logback.configurationFile"] = File(projectDir, "src/test/resources/logback-test.xml").absolutePath
 }
 dependencies {
-    val ktorVersion = "1.2.3"
+    val ktorVersion = "1.2.2"
 
     kapt("com.squareup.moshi:moshi-kotlin-codegen:1.8.0")
 
--- server/src/main/kotlin/HTTP.kt
+++ server/src/main/kotlin/HTTP.kt
@@ -1,6 +1,7 @@
 package edu.illinois.cs.cs125.jeed.server
 
 import com.ryanharter.ktor.moshi.moshi
+import com.squareup.moshi.JsonDataException
 import com.squareup.moshi.JsonEncodingException
 import edu.illinois.cs.cs125.jeed.server.moshi.Adapters
 import edu.illinois.cs.cs125.jeed.core.moshi.Adapters as JeedAdapters
@@ -10,12 +11,15 @@ import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CORS
 import io.ktor.features.ContentNegotiation
+import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.get
+import io.ktor.routing.options
 import io.ktor.routing.post
 import io.ktor.routing.routing
+import java.lang.IllegalArgumentException
 
 fun Application.jeed() {
     install(CORS) {
@@ -36,6 +40,8 @@ fun Application.jeed() {
                 val job = call.receive<Job>()
                 call.respond(job.run())
             } catch (e: JsonEncodingException) {
+                call.respond(HttpStatusCode.BadRequest)
+            } catch (e: JsonDataException) {
                 call.respond(HttpStatusCode.BadRequest)
             }
         }
