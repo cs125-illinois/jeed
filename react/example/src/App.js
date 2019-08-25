58ce35a42120ed39a173f0b9009f432ea3fa9877
Geoffrey Challen <challen@illinois.edu>
2019-08-25 00:39:12 -0500

    Working on terminal output formatting.

--- react/example/src/App.js
+++ react/example/src/App.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react'
 
+import Children from 'react-children-utilities'
+
 import { JeedResult } from 'jeed'
+
+import Container from '@material-ui/core/Container'
+
+const backend = process.env.REACT_APP_JEED_BACKEND
+
+class JeedDiv extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { clicked: false }
+    this.source = Children.onlyText(this.props.children).trim() + "\n"
+    this.job = {
+      tasks: props.tasks || [ "execute" ],
+    }
+    if (props.snippet) {
+      this.job.snippet = this.source
+    } else {
+      const filename = props.filename || "Example.java"
+      this.job.sources = {
+        filename: this.source
+      }
+    }
+  }
+  render () {
+    return (
+      <div>
+        <pre><code>{ this.source }</code></pre>
+        <pre><code>{ JSON.stringify(this.job, null, 2) }</code></pre>
+        <JeedResult backend={backend} job={this.job} />
+      </div>
+    )
+  }
+}
 
 export default class App extends Component {
   render () {
-    const backend = process.env.REACT_APP_JEED_BACKEND
     return (
-      <div>
-        <JeedResult backend={ backend } job={{
-          tasks: [ "execute" ],
-          snippet: `System.out.println("Hello, world!");`
-        }} />
-      </div>
+      <Container maxWidth="md">
+        <JeedDiv snippet="true">{`
+for (long i = 0; i < 10000000L; i++) {
+  System.out.println(i);
+}
+        `}</JeedDiv>
+      </Container>
     )
   }
 }
--- react/src/index.js
+++ react/src/index.js
@@ -33,10 +33,23 @@ function jeedBackendWrapper (WrappedComponent) {
   return JeedBackendWrapper
 }
 
+function run (backend, job) {
+  const cancellationHandle = axios.CancelToken.source()
+
+  return {
+    response: axios.post(backend, job, {
+      cancelToken: cancellationHandle.token
+    }),
+    cancel: () => {
+      cancellationHandle.cancel()
+    }
+  }
+}
+
 class JeedResultInner extends Component {
   constructor(props) {
     super(props)
-    this.state = { request: null, result: null }
+    this.state = { request: null, result: null, err: null, completed: false }
   }
 
   componentDidUpdate() {
@@ -53,14 +66,14 @@ class JeedResultInner extends Component {
 
     this.setState({ request })
     request.response.then(response => {
-      this.setState({ result: response.data })
+      this.setState({ result: response.data, completed: true })
     }).catch(err => {
-      console.error(err)
+      this.setState({ err, completed: true })
     })
   }
 
   componentWillUnmount() {
-    if (!this.state.request || this.state.response) {
+    if (!this.state.request || this.state.completed) {
       return
     }
     this.state.request.cancel()
@@ -68,32 +81,80 @@ class JeedResultInner extends Component {
 
   render() {
     const { config } = this.props.jeed
-    const { result } = this.state
+    const { result, err } = this.state
 
     if (!config) {
-      const { connecting } = this.props
-      return ( connecting || <div>Connecting...</div> )
+      const { Connecting } = this.props
+      return ( Connecting || <div>Connecting...</div> )
     }
     if (!result) {
-      return ( <pre>{ JSON.stringify(config, null, 2) }</pre> )
+      const { Running } = this.props
+      return ( Running || <div>Running...</div> )
     }
-    return ( <pre>{ JSON.stringify(result, null, 2) }</pre> )
+    const Output = this.props.output || TerminalOutput
+    return ( <Output job={ this.props.job } { ...this.state } /> )
   }
+}
+
+const TerminalOutput = (props) => {
+  const { job, result } = props
+  let output = ""
+
+  if (result.failed.snippet) {
+    const { errors } = result.failed.snippet
+    output += errors.map(error => {
+      const { line, column, message } = error
+      const originalLine = job.snippet.split("\n")[line - 1]
+      return `Line ${ line }: error: ${ message }
+${ originalLine }
+${ new Array(column).join(" ") }^`
+    })
+
+    const errorCount = Object.keys(errors).length
+    output += `
+${ errorCount } error${ errorCount > 1 ? "s" : "" }`
+  } else if (result.failed.compilation) {
+    const { errors } = result.failed.compilation
+    output += errors.map(error => {
+      const { source, line, column } = error.location
+      const originalLine = source === "" ?
+        job.snippet.split("\n")[line - 1] :
+        job.sources[source].split("\n")[line - 1]
+      const firstErrorLine = error.message.split("\n").slice(0, 1).join("\n")
+      const restError = error.message.split("\n").slice(1).filter(errorLine => {
+        if (source === "" && errorLine.trim().startsWith("location: class")) {
+          return false
+        } else {
+          return true
+        }
+      }).join("\n")
+      return `${ source === "" ? "Line " : source }${ line }: error: ${ firstErrorLine }
+${ originalLine }
+${ new Array(column).join(" ") }^
+${ restError }`
+    }).join("\n")
+    const errorCount = Object.keys(errors).length
+    output += `
+${ errorCount } error${ errorCount > 1 ? "s" : "" }`
+  } else if (result.failed.execution) {
+    output += result.failed.execution
+  }
+
+  if (Object.keys(result.failed).length === 0) {
+    if (result.completed.execution) {
+      output += result.completed.execution.outputLines.map(outputLine => {
+        return outputLine.line
+      }).join("\n")
+    }
+  }
+
+  return (
+    <pre>
+        { JSON.stringify(result, null, 2) }<br />
+        { output }
+    </pre>)
 }
 
 const JeedResult = jeedBackendWrapper(JeedResultInner)
 export { JeedResult }
-
-function run (backend, job) {
-  const cancellationHandle = axios.CancelToken.source()
-
-  return {
-    response: axios.post(backend, job, {
-      cancelToken: cancellationHandle.token
-    }),
-    cancel: () => {
-      cancellationHandle.cancel()
-    }
-  }
-}
 
