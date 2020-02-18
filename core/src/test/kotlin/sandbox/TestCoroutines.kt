fa061314a7858a25849fec987e7bf19bfe407e4b
Geoffrey Challen <challen@illinois.edu>
2020-02-17 22:10:34 -0600

    Better coroutine test.

--- core/src/test/kotlin/sandbox/TestCoroutines.kt
+++ core/src/test/kotlin/sandbox/TestCoroutines.kt
@@ -1,29 +1,43 @@
 package edu.illinois.cs.cs125.jeed.core.sandbox
 
 import edu.illinois.cs.cs125.jeed.core.Source
+import edu.illinois.cs.cs125.jeed.core.SourceExecutionArguments
 import edu.illinois.cs.cs125.jeed.core.execute
 import edu.illinois.cs.cs125.jeed.core.haveCompleted
+import edu.illinois.cs.cs125.jeed.core.haveOutput
+import edu.illinois.cs.cs125.jeed.core.haveTimedOut
 import edu.illinois.cs.cs125.jeed.core.kompile
+import io.kotlintest.should
 import io.kotlintest.shouldNot
 import io.kotlintest.specs.StringSpec
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.util.PropertyPermission
 
 class TestCoroutines : StringSpec({
     "should prevent coroutines from escaping the sandbox" {
+        // Dummy task to force GlobalScope to be initialized before the test runs
+        var i = 0
+        GlobalScope.launch { i = 1 }
         val executionResult = Source(mapOf(
             "Main.kt" to """
 import kotlinx.coroutines.*
 
 fun main() {
-    GlobalScope.launch {
-        delay(10L)
-        println("World!")
+    val job = GlobalScope.launch {
+        println("Hello")
     }
-    println("Hello")
     Thread.sleep(50L)
+    println("World!")
 }
 """.trimIndent()
-        )).kompile().execute()
-
-        executionResult shouldNot haveCompleted()
+        )).kompile().execute(executionArguments = SourceExecutionArguments(
+            maxExtraThreads = 4,
+            permissions = setOf(PropertyPermission("java.specification.version", "read"))
+        ))
+        executionResult shouldNot haveTimedOut()
+        executionResult should haveOutput("Hello\nWorld!")
     }
 })
