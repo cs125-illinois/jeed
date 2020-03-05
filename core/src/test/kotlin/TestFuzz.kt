07095d5594ef665484f633338fabe73df73739de
arjunvnair <arjunvnair@hotmail.com>
2020-03-05 10:36:51 -0600

    Create test for increment mutator

--- core/src/test/kotlin/TestFuzz.kt
+++ core/src/test/kotlin/TestFuzz.kt
@@ -4,6 +4,8 @@ import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
 class TestFuzz : StringSpec({
+    // Conditional Boundary
+
     "conditional_boundary (all)" {
         val source = """
 boolean a = 1 > 0; 
@@ -21,6 +23,32 @@ boolean d = 4 > 4;
         fuzzConfiguration.conditionals_boundary = true
         fuzzConfiguration.conditionals_boundary_rand = false
         val fuzzedSource = fuzzBlock(source, fuzzConfiguration)
+        println(fuzzedSource)
+        fuzzedSource shouldBe expectedFuzzedSource
+    }
+
+    // Increment
+
+    "increment (all)" {
+        val source = """
+int foo = 5;
+for (int i = 0; i < 5; i++) {
+    foo--;
+}
+++foo;
+""".trim()
+        val expectedFuzzedSource = """
+int foo = 5;
+for (int i = 0; i < 5; i--) {
+    foo++;
+}
+--foo;
+""".trim()
+        val fuzzConfiguration = FuzzConfiguration()
+        fuzzConfiguration.increment = true
+        fuzzConfiguration.increment_rand = false
+        val fuzzedSource = fuzzBlock(source, fuzzConfiguration)
+        println(fuzzedSource)
         fuzzedSource shouldBe expectedFuzzedSource
     }
 })
