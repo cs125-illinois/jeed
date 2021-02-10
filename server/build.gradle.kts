40847e6c3511d19164e94d0e90483a4c622ee0b0
Geoffrey Challen <challen@illinois.edu>
2021-02-10 10:51:09 -0600

    Update several dependenices.

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     implementation("io.github.microutils:kotlin-logging:2.0.4")
     implementation("com.google.api-client:google-api-client:1.31.2")
 
-    val kotestVersion = "4.4.0"
+    val kotestVersion = "4.4.1"
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-ktor:$kotestVersion")
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
