5db9dc88b03f269db0a849fba2a871ad9e6750e8
Geoffrey Challen <challen@illinois.edu>
2021-12-23 15:42:29 -0600

    Bump version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ allprojects {
 }
 subprojects {
     group = "com.github.cs125-illinois.jeed"
-    version = "2021.12.2"
+    version = "2021.12.3"
     tasks.withType<Test> {
         useJUnitPlatform()
         enableAssertions = true
