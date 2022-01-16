6477139639987df87695d506f0192f7a458d056c
Ben Nordick <ben@rigeltechnical.com>
2022-01-16 15:18:15 -0500

    Trace method arguments

--- core/src/main/kotlin/ExecutionTrace.kt
+++ core/src/main/kotlin/ExecutionTrace.kt
@@ -1,8 +1,27 @@
 package edu.illinois.cs.cs125.jeed.core
 
 import com.squareup.moshi.JsonClass
+import org.objectweb.asm.ClassReader
+import org.objectweb.asm.ClassWriter
+import org.objectweb.asm.Handle
+import org.objectweb.asm.Opcodes
+import org.objectweb.asm.Type
+import org.objectweb.asm.tree.ClassNode
+import org.objectweb.asm.tree.InsnList
+import org.objectweb.asm.tree.InvokeDynamicInsnNode
+import org.objectweb.asm.tree.LabelNode
+import org.objectweb.asm.tree.MethodNode
+import org.objectweb.asm.tree.VarInsnNode
+import java.lang.invoke.CallSite
+import java.lang.invoke.ConstantCallSite
+import java.lang.invoke.MethodHandles
+import java.lang.invoke.MethodType
+import kotlin.reflect.jvm.javaMethod
 
 object ExecutionTrace : SandboxPluginWithDefaultArguments<ExecutionTraceArguments, ExecutionTraceResults> {
+    private val supportClassName = classNameToPath(TracingSupport::class.java.name)
+    private val methodEnterBootstrapDesc = Type.getMethodDescriptor(TracingSupport::bootstrapEnterMethod.javaMethod)
+
     private val threadData: ThreadLocal<ExecutionTraceWorkingData> = ThreadLocal.withInitial {
         Sandbox.CurrentTask.getWorkingData(this)
     }
@@ -20,6 +39,64 @@ object ExecutionTrace : SandboxPluginWithDefaultArguments<ExecutionTraceArgument
         return ExecutionTraceInstrumentationData(arguments, hasLineTrace)
     }
 
+    override fun transformBeforeSandbox(
+        bytecode: ByteArray,
+        name: String,
+        instrumentationData: Any?,
+        context: RewritingContext
+    ): ByteArray {
+        if (context != RewritingContext.UNTRUSTED) return bytecode
+        instrumentationData as ExecutionTraceInstrumentationData
+        val reader = ClassReader(bytecode)
+        val classTree = ClassNode(Opcodes.ASM9)
+        reader.accept(classTree, 0)
+        classTree.methods.forEach {
+            instrumentMethod(instrumentationData, classTree.name, it)
+        }
+        val writer = ClassWriter(reader, ClassWriter.COMPUTE_MAXS)
+        classTree.accept(writer)
+        return writer.toByteArray()
+    }
+
+    private fun instrumentMethod(data: ExecutionTraceInstrumentationData, className: String, method: MethodNode) {
+        if (method.instructions.size() == 0) return // Can't instrument an abstract method
+        val methodStart = method.instructions.first { it is LabelNode } as? LabelNode ?: return // No locals
+        val methodIndex = data.nextMethodIndex
+        data.nextMethodIndex++
+        val methodKey = "m$methodIndex"
+        val localVariables = method.localVariables ?: listOf()
+        val passableArguments = localVariables.filter {
+            it.start == methodStart && (it.index != 0 || method.name != "<init>")
+        }.sortedBy {
+            it.index
+        }.map {
+            ExecutionTraceInstrumentationData.ArgumentInfo(it.index, it.name, Type.getType(it.desc))
+        }
+        val methodInfo = ExecutionTraceInstrumentationData.MethodInfo(
+            methodIndex,
+            className,
+            method.name,
+            method.desc,
+            passableArguments,
+            method.access.and(Opcodes.ACC_STATIC) == 0 && method.name != "<init>"
+        )
+        data.instrumentedMethods[methodKey] = methodInfo
+        val enterMethodCall = InsnList()
+        passableArguments.forEach {
+            enterMethodCall.add(VarInsnNode(it.type.getOpcode(Opcodes.ILOAD), it.localIndex))
+        }
+        val enterMethodHandle = Handle(
+            Opcodes.H_INVOKESTATIC,
+            supportClassName,
+            "bootstrapEnterMethod",
+            methodEnterBootstrapDesc,
+            false
+        )
+        val enterMethodDesc = Type.getMethodDescriptor(Type.VOID_TYPE, *passableArguments.map { it.type }.toTypedArray())
+        enterMethodCall.add(InvokeDynamicInsnNode(methodKey, enterMethodDesc, enterMethodHandle))
+        method.instructions.insert(enterMethodCall)
+    }
+
     override fun createInitialData(instrumentationData: Any?, executionArguments: Sandbox.ExecutionArguments): Any {
         require(executionArguments.maxExtraThreads == 0) { "ExecutionTrace does not support multiple threads" }
         return ExecutionTraceWorkingData(instrumentationData as ExecutionTraceInstrumentationData)
@@ -29,7 +106,7 @@ object ExecutionTrace : SandboxPluginWithDefaultArguments<ExecutionTraceArgument
         val workingData: ExecutionTraceWorkingData = Sandbox.CurrentTask.getWorkingData(this)
         if (workingData.instrumentationData.hasLineTrace) {
             LineTrace.addLineCallback { source, line ->
-                if (workingData.steps.size < workingData.instrumentationData.arguments.recordedStepLimit) {
+                if (!workingData.atCapacity()) {
                     workingData.steps.add(ExecutionStep.Line(source, line))
                 }
             }
@@ -39,6 +116,38 @@ object ExecutionTrace : SandboxPluginWithDefaultArguments<ExecutionTraceArgument
     override fun createFinalData(workingData: Any?): ExecutionTraceResults {
         workingData as ExecutionTraceWorkingData
         return ExecutionTraceResults(workingData.instrumentationData.arguments, workingData.steps)
+    }
+
+    override val requiredClasses: Set<Class<*>>
+        get() = setOf(TracingSupport::class.java)
+
+    object TracingSupport {
+        private val lookup = MethodHandles.lookup()
+        private val enterMethodHandle = lookup.unreflect(TracingSupport::enterMethod.javaMethod)
+
+        @JvmStatic
+        @Suppress("UNUSED_PARAMETER") // The first MethodHandles.Lookup parameter is required by the JVM
+        fun bootstrapEnterMethod(caller: MethodHandles.Lookup, methodKey: String, callSignature: MethodType): CallSite {
+            val handle = enterMethodHandle
+                .bindTo(methodKey)
+                .asCollector(Array<Any>::class.java, callSignature.parameterCount())
+                .asType(MethodType.methodType(Void.TYPE, callSignature.parameterArray()))
+            return ConstantCallSite(handle)
+        }
+
+        @JvmStatic
+        private fun enterMethod(methodKey: String, passableArguments: Array<Any?>) {
+            val data = threadData.get()
+            if (data.atCapacity()) return
+            val methodInfo = data.instrumentationData.instrumentedMethods[methodKey] ?: error("uninstrumented method")
+            val receiver = if (methodInfo.local0IsReceiver) passableArguments[0] else null
+            val passedArgumentInfo = methodInfo.passableArguments.zip(passableArguments).filter { (iai, _) ->
+                !methodInfo.local0IsReceiver || iai.localIndex > 0
+            }.map { (iai, value) ->
+                ExecutionTraceResults.PassedArgument(iai.name, value)
+            }
+            data.steps.add(ExecutionStep.EnterMethod(methodInfo.asPublishableInfo(), receiver, passedArgumentInfo))
+        }
     }
 }
 
@@ -53,24 +162,61 @@ data class ExecutionTraceArguments(
 
 private class ExecutionTraceInstrumentationData(
     val arguments: ExecutionTraceArguments,
-    val hasLineTrace: Boolean
-)
+    val hasLineTrace: Boolean,
+    var nextMethodIndex: Int = 1,
+    val instrumentedMethods: MutableMap<String, MethodInfo> = mutableMapOf()
+) {
+    data class MethodInfo(
+        val index: Int,
+        val className: String,
+        val name: String,
+        val descriptor: String,
+        val passableArguments: List<ArgumentInfo>,
+        val local0IsReceiver: Boolean
+    ) {
+        fun asPublishableInfo(): ExecutionTraceResults.MethodInfo {
+            val arguments = passableArguments
+                .filter { !local0IsReceiver || it.localIndex > 0 }
+                .map { it.type.toString() }
+            return ExecutionTraceResults.MethodInfo(className, name, arguments)
+        }
+    }
+
+    data class ArgumentInfo(val localIndex: Int, val name: String, val type: Type)
+}
 
 private class ExecutionTraceWorkingData(
     val instrumentationData: ExecutionTraceInstrumentationData,
     val steps: MutableList<ExecutionStep> = mutableListOf()
-)
+) {
+    fun atCapacity(): Boolean {
+        return steps.size >= instrumentationData.arguments.recordedStepLimit
+    }
+}
 
 @JsonClass(generateAdapter = true)
 data class ExecutionTraceResults(
     val arguments: ExecutionTraceArguments,
     val steps: List<ExecutionStep>
-)
+) {
+    @JsonClass(generateAdapter = true)
+    data class MethodInfo(val className: String, val method: String, val argumentTypes: List<String>)
 
-enum class ExecutionStepType(val stepClass: Class<out ExecutionStep>) {
-    LINE(ExecutionStep.Line::class.java)
+    @JsonClass(generateAdapter = true)
+    data class PassedArgument(val argumentName: String, val value: Any?)
 }
 
-sealed class ExecutionStep(type: ExecutionStepType) {
+@Suppress("unused") // stepClass is for serialization
+enum class ExecutionStepType(val stepClass: Class<out ExecutionStep>) {
+    LINE(ExecutionStep.Line::class.java),
+    ENTER_METHOD(ExecutionStep.EnterMethod::class.java)
+}
+
+sealed class ExecutionStep(val type: ExecutionStepType) {
     data class Line(val source: String, val line: Int) : ExecutionStep(ExecutionStepType.LINE)
+    data class EnterMethod(
+        val method: ExecutionTraceResults.MethodInfo,
+        val receiver: Any?,
+        val arguments: List<ExecutionTraceResults.PassedArgument>
+    ) : ExecutionStep(ExecutionStepType.ENTER_METHOD)
 }
--- core/src/test/kotlin/TestExecutionTrace.kt
+++ core/src/test/kotlin/TestExecutionTrace.kt
@@ -1,7 +1,13 @@
 package edu.illinois.cs.cs125.jeed.core
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.beEmpty
 import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.nulls.beNull
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNot
+import io.kotest.matchers.types.beInstanceOf
 
 class TestExecutionTrace : StringSpec({
     "should register for line trace events" {
@@ -18,5 +24,82 @@ public class Main {
             .execute(SourceExecutionArguments().addPlugin(LineTrace).addPlugin(ExecutionTrace))
         val executionTrace = result.pluginResult(ExecutionTrace)
         executionTrace.steps shouldContain ExecutionStep.Line("Main.java", 3)
+    }
+
+    "should record method entry" {
+        val result = Source.fromJava(
+            """
+public class Main {
+  public static void main() {
+    int i = 4;
+    i += 1;
+    System.out.println(i);
+  }
+}""".trim()
+        ).compile(CompilationArguments(debugInfo = true))
+            .execute(SourceExecutionArguments().addPlugin(ExecutionTrace))
+        val executionTrace = result.pluginResult(ExecutionTrace)
+        executionTrace.steps[0] should beInstanceOf<ExecutionStep.EnterMethod>()
+        val methodStep = executionTrace.steps[0] as ExecutionStep.EnterMethod
+        methodStep.method.method shouldBe "main"
+        methodStep.receiver should beNull()
+        methodStep.arguments should beEmpty()
+    }
+
+    "should record method entry with arguments" {
+        val result = Source.fromSnippet(
+            """
+void printInt(int number) {
+    System.out.println(number);
+}
+void printSum(short a, short b) {
+    printInt(a + b);
+}
+printSum((short) 6, (short) 4);
+""".trim()
+        ).compile(CompilationArguments(debugInfo = true))
+            .execute(SourceExecutionArguments().addPlugin(ExecutionTrace))
+        result should haveCompleted()
+        result should haveOutput("10")
+        val executionTrace = result.pluginResult(ExecutionTrace)
+        val methodSteps = executionTrace.steps.filterIsInstance<ExecutionStep.EnterMethod>()
+        methodSteps[1].method.method shouldBe "printSum"
+        methodSteps[1].receiver should beNull()
+        methodSteps[1].arguments.size shouldBe 2
+        methodSteps[1].arguments[0].argumentName shouldBe "a"
+        methodSteps[1].arguments[0].value shouldBe 6
+        methodSteps[1].arguments[1].argumentName shouldBe "b"
+        methodSteps[2].method.method shouldBe "printInt"
+        methodSteps[2].receiver should beNull()
+        methodSteps[2].arguments.size shouldBe 1
+        methodSteps[2].arguments[0].argumentName shouldBe "number"
+        methodSteps[2].arguments[0].value shouldBe 10
+    }
+
+    "should record method entry with receivers" {
+        val result = Source.fromSnippet(
+            """
+class Adder {
+    private int base;
+    Adder(int setBase) {
+        base = setBase;
+    }
+    int add(int plus) {
+        return base + plus;
+    }
+}
+Adder adder = new Adder(5);
+System.out.println(adder.add(2));
+""".trim()
+        ).compile(CompilationArguments(debugInfo = true))
+            .execute(SourceExecutionArguments().addPlugin(ExecutionTrace))
+        result should haveCompleted()
+        result should haveOutput("7")
+        val executionTrace = result.pluginResult(ExecutionTrace)
+        val methodSteps = executionTrace.steps.filterIsInstance<ExecutionStep.EnterMethod>()
+        val addStep = methodSteps.find { it.method.className == "Adder" && it.method.method == "add" }!!
+        addStep.arguments[0].argumentName shouldBe "plus"
+        addStep.arguments[0].value shouldBe 2
+        addStep.receiver shouldNot beNull()
     }
 })
