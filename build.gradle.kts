969b60609ad4d8042f39232bca2d65da0259eedb
Ben Nordick <ben@rigeltechnical.com>
2022-05-30 15:09:12 -0400

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ allprojects {
 }
 subprojects {
     group = "com.github.cs125-illinois.jeed"
-    version = "2022.5.0"
+    version = "2022.5.1"
     tasks.withType<Test> {
         useJUnitPlatform()
         enableAssertions = true
