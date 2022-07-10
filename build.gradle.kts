c088f29a18068fbe5afeac759a674c442c1f2d33
Geoffrey Challen <challen@illinois.edu>
2022-07-10 11:07:31 -0500

    Start next version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ allprojects {
 }
 subprojects {
     group = "com.github.cs125-illinois.jeed"
-    version = "2022.7.0"
+    version = "2022.7.1"
     tasks.withType<Test> {
         useJUnitPlatform()
         enableAssertions = true
