1a3b5f0b838960a8bffd606c39539d191644e473
Geoffrey Challen <challen@illinois.edu>
2022-07-17 13:51:09 -0500

    Bump verison for next release.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ allprojects {
 }
 subprojects {
     group = "com.github.cs125-illinois.jeed"
-    version = "2022.7.1"
+    version = "2022.7.2"
     tasks.withType<Test> {
         useJUnitPlatform()
         enableAssertions = true
