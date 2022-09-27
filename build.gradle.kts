9d01a8e0e83dcccd3bddbc96edc9fc7899ab9b8e
Geoffrey Challen <challen@illinois.edu>
2022-09-27 14:50:47 -0500

    Pushing small stack trace fix.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ allprojects {
 }
 subprojects {
     group = "com.github.cs125-illinois.jeed"
-    version = "2022.9.5"
+    version = "2022.9.6"
     tasks.withType<Test> {
         useJUnitPlatform()
         enableAssertions = true
