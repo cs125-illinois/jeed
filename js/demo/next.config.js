790a4f93bb350d97031fd810fd861efb0371e443
Geoffrey Challen <challen@illinois.edu>
2022-10-24 15:17:36 -0500

    Update dependencies and bump versions.

--- js/demo/next.config.js
+++ js/demo/next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? "",
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ?? "",
+  ...(process.env.NEXT_PUBLIC_BASE_PATH && { assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH }),
   reactStrictMode: true,
 }
